f8f8b20cfc13cdd3ef8af292ba10414d255c8866
Kamyab Rouhifar <krouhifar@myseneca.ca>
2024-02-16 01:43:52 -0500

    updated shell script

--- zip_files.sh
+++ zip_files.sh
@@ -3,6 +3,6 @@
 output="archive.zip"
 
 # Zip the file or directory
-zip -r $output ./* -x "$output" "./zip_files.sh" "./README.md"
+zip -r $output ./* -x "$output" "./zip_files.sh" "./README.md" "./.*"
 
 echo "all files has been zipped into '$output'"
