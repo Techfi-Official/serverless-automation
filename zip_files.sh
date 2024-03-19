7e38c59e859d02311d8f3fd752ac5e7bdd956de0
Kamyab Rouhifar <krouhifar@myseneca.ca>
2024-03-19 16:22:38 -0400

    added python model

--- zip_files.sh
+++ zip_files.sh
@@ -3,6 +3,7 @@
 output="archive.zip"
 
 # Zip the file or directory
-zip -r $output ./* -x "$output" "./zip_files.sh" "./README.md" "./.*"
+zip -r archive.zip ./* -x "archive.zip" "./zip_files.sh" "./README.md" "./.*" "public/*" "model_AI/*"
+
 
 echo "all files has been zipped into '$output'"
