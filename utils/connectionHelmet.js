07f0647533ae397376d368a6ccb9f563433c5ab3
Kamyab Rouhifar <krouhifar@myseneca.ca>
2024-05-05 23:32:50 -0400

    updated changes

--- utils/connectionHelmet.js
+++ utils/connectionHelmet.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'connect-src': ["'self'", 'https://reqres.in/api/users?page=2'],
+    'connect-src': ["'self'", 'https://reqres.in/api/users?page=2', 'https://a2pejekyml.execute-api.us-east-1.amazonaws.com/PROD/post-data-image-ai'],
     'img-src': [
         "'self'",
         'data:',
