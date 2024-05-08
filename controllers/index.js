761d6072a10a9bbbdd73e1781fe4995687a39d5a
Kamyab Rouhifar <krouhifar@myseneca.ca>
2024-05-07 22:08:11 -0400

    updated testing

--- controllers/index.js
+++ controllers/index.js
@@ -13,6 +13,10 @@ router.get(['/'], getDataRenderHTML)
 
 router.get(['/wake-ai-model'], authUserApiKey, wakeAIModel)
 
+router.get(['/test'], authUserApiKey, (req, res) => {
+    res.status(200).send('OK')
+})
+
 router.post(['/post-data-image-ai'], authUserApiKey, postDataAndImageAI)
 
 router.post(['/send-email'], authUserApiKey, sendEmail)
