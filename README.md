9854c06ee51b3423aeb4251de19570c04997df60
Ozan <muldurozan@gmail.com>
2024-09-29 23:58:17 -0400

    content security policy fixed

--- README.md
+++ README.md
@@ -3,5 +3,10 @@ http://localhost:3000/?id=56115192-2a1b-4d52-a43e-efb7a6a3a758
 
 http://localhost:3000/?id=techfi1992
 http://localhost:3000/?id=techfi1992&scheduleID=bmQ7i403X4yE7Rp2
+https://a2pejekyml.execute-api.us-east-1.amazonaws.com/dev/?id=techfi1992&scheduleID=bmQ7i403X4yE7Rp2
 lambda function 
 https://a2pejekyml.execute-api.us-east-1.amazonaws.com/PROD/?id=56115192-2a1b-4d52-a43e-efb7a6a3a758
+
+
+
+https://a2pejekyml.execute-api.us-east-1.amazonaws.com/dev/?id=techfi1992&scheduleID=TNCRKn5XkTqk1Ylq
--- utils/connectionHelmet.js
+++ utils/connectionHelmet.js
@@ -3,13 +3,16 @@ module.exports = {
         "'self'",
         'https://a2pejekyml.execute-api.us-east-1.amazonaws.com/PROD/post-data-image-ai',
         'https://cloud.activepieces.com',
+        'https://automation-media.s3.amazonaws.com', // Allow S3 access
     ],
     'img-src': [
         "'self'",
         'data:',
         'blob:',
+        'https://automation-media.s3.amazonaws.com', // Allow images from your S3 bucket
         'https://d39oted90y97ty.cloudfront.net',
         'https://idsb.tmgrup.com.tr/ly/uploads/images/2023/11/14/301015.jpg',
+        'https://idsb.tmgrup.com.tr/ly/uploads/images/', // Example external image link
     ],
     'script-src': [
         "'self'",
@@ -24,5 +27,6 @@ module.exports = {
         'https://cdn.jsdelivr.net/npm/sweetalert2@11.10.8/dist/sweetalert2.min.css',
         'https://cdn.jsdelivr.net/npm/sweetalert2@11.10.8/dist/sweetalert2.all.min.js',
         'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css',
+        'https://cdn.jsdelivr.net/npm/sweetalert2@11.10.8/dist/sweetalert2.all.min.js'
     ],
 }
