86a390aa229b80992c4b4d14608214d864aefa44
Ozan <muldurozan@gmail.com>
2024-10-02 18:01:36 -0400

    url is being used now

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -435,7 +435,7 @@ module.exports.proxy = async (req, res) => {
     } 
 
     try {
-        const response = await fetch(process.env.API_URL+`?id=${clientID}&scheduleID=${scheduleID}`, {
+        const response = await fetch(url, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(req.body),
